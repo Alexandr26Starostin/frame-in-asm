3ed0efa3112835953486909573f300936fb18b5e
Alexandr26Starostin <sanchostarostin@gmail.com>
2025-02-10 07:08:21 +0300

    Version 4: can print 'SYMBOL' with 'COLOR' in point(X_0, Y_0) and uses variables for it

--- frame.asm
+++ frame.asm
@@ -6,7 +6,21 @@
 ;                                        program
 
 .model tiny   ;64 kilobytes in RAM, address == 16 bit == 2 bytes (because: sizeof (register) == 2 bytes)
+
+;--------------------------------------------------------------------------------------------------------------
+;                                      variables
+X_0 = 40d    
+Y_0 = 5d
+LEN_STR = 80d
+COLOR = 11011010b
+	   ;bBBBFFFF	b == blink;  B == back ground;  F == for ground       
+	   ; rgbIRGB	r/R == red;  g/G == green;  b/B == blue;  I == increase
+
+SYMBOL = 'A'
+;--------------------------------------------------------------------------------------------------------------
+
 .code         ;begin program
+
 org 100h      ;START == 256:   jmp START == jmp 256 != jmp 0 (because address [0;255] in program segment in DOS for PSP)
 START:
 	call Put_Sym       ;call func
@@ -16,10 +30,9 @@ START:
 ;--------------------------------------------------------------------------------------------------------------
 
 
-
 ;--------------------------------------------------------------------------------------------------------------
 ;											 Put_Sym
-;Draws one char to video memory in (x = X_0, y = Y_0)
+;Draws one char 'SYMBOL' with 'COLOR' to video memory in (x = X_0, y = Y_0)
 ;Entry: None
 ;Exit:  None
 ;Destr: bx, es
@@ -29,37 +42,19 @@ Put_Sym proc           ;mark for func
 	mov bx, 0b800h     ;video segment
 	mov es, bx         ;register es for segment address of video memory  (es != const    es == reg)
 
-	mov bx, 5 * 80 * 2 + 40 * 2         ;offset in video segment  (2 <= 2 bytes)
-
-	;!!! push ax       ;use ax
+	mov bx, Y_0 * 80 * 2 + X_0 * 2         ;offset in video segment  (2 <-- 2 bytes)
 
 	;in text mode in video memory: sizeof (symbol) == 2
 	;byte ptr == mov 1 byte  in memory
 	;word ptr == mov 2 bytes in memory
 
-	;!!! mov ax, SYMBOL
-	mov byte ptr es:[bx],   'A'  ;first byte for symbol's ASCII code
-	;!!! mov ax, COLOR
-	mov byte ptr es:[bx+1], 11011010b  ;second byte for symbol's color  
-
-	;!!! pop ax      ;save ax
+	mov byte ptr es:[bx],   SYMBOL  ;first byte for symbol's ASCII code
+	mov byte ptr es:[bx+1], COLOR   ;second byte for symbol's color  
 
 	ret     ;refund of control
 	endp    ;end of func
 ;--------------------------------------------------------------------------------------------------------------
 
-
-;--------------------------------------------------------------------------------------------------------------
-;                                      variables
-;X_0: dw 40    
-;Y_0: dw 5
-;LEN_STR: dw 80
-;COLOR: db 11011010b
-		;bBBBFFFF	b == blink;  B == back ground;  F == for ground       
-		; rgbIRGB	r/R == red;  g/G == green;  b/B == blue;  I == increase
-
-;SYMBOL: db 'A'
-;--------------------------------------------------------------------------------------------------------------
 
 end START              ;end of asm and address of program's beginning
 
