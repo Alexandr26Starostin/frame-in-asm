688342a03432b872f26d86483b8057a345039153
Alexandr26Starostin <sanchostarostin@gmail.com>
2025-02-14 00:36:03 +0300

    Version 9: draws frame with various color (uses atob) and with various style

--- frame.asm
+++ frame.asm
@@ -15,7 +15,7 @@ color db 00000000b
 x_size dw 0000d   ;horizontal sizes of frame
 y_size dw 0000d   ;vertical   sizes of frame
 
-frame_style db '123456789$'
+frame_style db '123456789+-+I I+-+$'
 
 ;--------------------------------------------------------------------------------------------------------------
 ;										main program
@@ -33,20 +33,26 @@ start:
 	mov bx, 0081h      ;bx = address of the first symbol in command line
 
 	call skip_spaces
-
 	call atoi
 	mov x_size, ax     ;x_size = ax = horizontal size of frame
 
 	call skip_spaces
-
 	call atoi
 	mov y_size, ax     ;y_size = ax = vertical   size of frame
 
+	call skip_spaces
+	call atob
+	mov color, al      ;color = al = color of symbols in frame
+
+
+	push bx  ;save bx = address of symbol in command line
 	call count_left_high_point   
+	pop bx
 
-	call atob     ;ah = color       
+	call skip_spaces
+	call evaluate_address_of_beginning_style   ;si = address on style
 
-	mov si, offset frame_style  ;si = address on line with style               
+	mov ah, color     ;ah = color                
 	mov cx, x_size
 	sub cx, 0002d        ;cx = x_size - 2 = len of str with recurring symbol 
 
@@ -56,13 +62,10 @@ start:
 	int 21h            ;call system
 ;--------------------------------------------------------------------------------------------------------------
 
-
-
-
 ;--------------------------------------------------------------------------------------------------------------
 ;											 atoi
 ;translate str to 2 bytes int (dec number)
-
+;
 ;Entry: bx = address of symbol in command line
 ;
 ;Exit:  ax = int number from str
@@ -76,7 +79,7 @@ start:
 
 atoi proc      ;mov ax, 0030d   
 
-	mov ax, 0000d   ;ax = 0
+	xor ax, ax   ;ax = 0
 
 	check_next_symbol_in_atoi:
 
@@ -85,7 +88,7 @@ atoi proc      ;mov ax, 0030d
 	cmp cl, '0'     ;cl - '0' < 0 => end_atoi
 	js end_atoi
 
-	cmp cl, '9'     ;'9' - cl < 0 => end_atoi
+	cmp cl, ':'     ;':' - cl < 0 => end_atoi     ':' = '0' + 10
 	jns end_atoi
 
 	mov dx, 0010d
@@ -103,8 +106,6 @@ atoi proc      ;mov ax, 0030d
 	ret     
 	endp   
 ;--------------------------------------------------------------------------------------------------------------
-
-
 
 ;--------------------------------------------------------------------------------------------------------------
 ;											 count_left_high_point
@@ -150,27 +151,50 @@ count_left_high_point proc
 	endp   
 ;--------------------------------------------------------------------------------------------------------------
 
-
-
 ;--------------------------------------------------------------------------------------------------------------
 ;											 atob
-;translate str to 1 byte int (bin number) (but it can not do this operation on this version)
-
-;Entry: None (on this version)
+;translate str to 2 bytes int (bin number)
 ;
-;Exit:  ah = int number from str
+;Entry: bx = address of symbol in command line
 ;
-;Destr: ah = reading int from str
+;Exit:  ax = int number from str
+;       bx = address of symbol in command line; symbol != '0-1' (not a digit)
+;
+;Destr: ax = reading int from str
+;		bx = shifting address of symbol in command line before it finds '0-1'
+;       cx = read 1 digit and participates in calculations
+;		dx = for calculations
 ;--------------------------------------------------------------------------------------------------------------
 
-atob proc          
-	mov ah, 01011011b
+atob proc      ;mov ax, 0030d   
+
+	xor ax, ax   ;ax = 0	
+
+	check_next_symbol_in_atob:
+
+	mov cl, ds:[bx]  ;cl = [bx]
+
+	cmp cl, '0'     ;cl - '0' < 0 => end_atob
+	js end_atob
+
+	cmp cl, '2'     ;'2' - cl < 0 => end_atob
+	jns end_atob
+
+	mov dx, 0002d
+	mul dx       ;ax = ax * 10
+
+	sub cl, '0'     ;cl = cl - '0'  =>  '<digital>' --> <digital>
+	add ax, cx      ;ax = (ax * 10) + cl - '0'
+
+	inc bx          ;bx++
+
+	jmp check_next_symbol_in_atob
+
+	end_atob:
 
 	ret     
-	endp   
+	endp     
 ;--------------------------------------------------------------------------------------------------------------
-
-
 
 ;--------------------------------------------------------------------------------------------------------------
 ;											 print_frame
@@ -184,35 +208,34 @@ atob proc
 ;
 ;Exit: None 
 ;
-;Destr: bx = index of lines
+;Destr: dx = index of lines
 ;		si = address on different sets of symbols from line with style 
 ;--------------------------------------------------------------------------------------------------------------
 
 print_frame proc          
-	mov bx, y_size        ;bx - index of line  (max value = y_size, step = -1, min value = 0)
+	mov dx, y_size        ;dx - index of line  (max value = y_size, step = -1, min value = 0)
 
 	call print_line       ;draw the first line of frame with the first set of symbols
-	dec bx        
+	dec dx        
 
 	print_new_line:       ;for (bx = y_size - 1; bx > 1; bx--) {printf ("%s\n", middle_line);}
 
 		push si           ;save address on the second set of symbols (for the middle lines)
 		call print_line   ;draw the middle line
-		dec bx
+		dec dx
 		pop si             
 
-		cmp bx, 1d
+		cmp dx, 1d
 		jnz print_new_line
 
 	add si, 3            ;address on the third set of symbols (for the last line)
 
 	call print_line      ;draw the last line of frame
-	dec bx
+	dec dx
 
 	ret     
 	endp   
 ;--------------------------------------------------------------------------------------------------------------
-
 
 ;--------------------------------------------------------------------------------------------------------------
 ;											 print_line
@@ -264,9 +287,6 @@ print_line proc
 	endp    
 ;--------------------------------------------------------------------------------------------------------------
 
-
-
-
 ;--------------------------------------------------------------------------------------------------------------
 ;											 skip_spaces
 ;skips spaces (' ') while doesn't find other symbol
@@ -299,10 +319,32 @@ skip_spaces proc
 
 	ret     
 	endp   
+;-------------------------------------------------------------------------------------------------------------- 
+
+;--------------------------------------------------------------------------------------------------------------
+;										evaluate_address_of_beginning_style
+;find address on the beginning of our style 
+;
+;Entry: bx = address of symbol in command line
+;
+;Exit:  si = address on line with our style
+;
+;Destr: si = for evaluating of our address
+;		ax = for calculations: has the number of style --> has shifting for beginning of our style  
+;		bx = address shifting (destr in atoi)
 ;--------------------------------------------------------------------------------------------------------------
 
+evaluate_address_of_beginning_style proc   
 
+	call atoi    ;ax = number of style
 
+	mov si, 0009d
+	mul si              ;ax = ax * 9d (every style has 9 symbols => shifting for beginning of our style = number_of_style * 9 symbols)
 
+	mov si, offset frame_style   ;si = address on line with all styles
+	add si, ax                   ;si = &frame_style + 9 * number_style
 
+	ret     
+	endp   
+;--------------------------------------------------------------------------------------------------------------
 end start              ;end of asm and address of program's beginning
